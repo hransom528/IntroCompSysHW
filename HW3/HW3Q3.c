839b8a4852681fd5d1e48e13d965f56f1647ee0d
Harris Ransom <hransom528@protonmail.com>
2024-03-20 22:10:42 -0400

    Added HW3 code

--- HW3/HW3Q3.c
+++ HW3/HW3Q3.c
@@ -5,29 +5,36 @@
 #include <sys/types.h>
 #include <signal.h>
 
+#define FLAGS1 WUNTRACED | WCONTINUED
+#define FLAGS2 WUNTRACED | WCONTINUED | WNOHANG
+#define FLAGS3 WNOHANG
+#define CONDITIONALS1 (!WIFEXITED(status))&&(!WIFSIGNALED(status))
+#define CONDITIONALS2 ((!w)||((!WIFEXITED(status))&&(!WIFSIGNALED(status))))
+
+// Main
 int main(int argc, char *argv[]) {
     int i, status,parent_PID;
     pid_t pid, cpid, w;
     cpid = fork();
     
     if (cpid<0) return 1;
-    else if (cpid ==0) {
+    else if (cpid == 0) {
         sleep (atoi(argv[2])); //assume argv[2] > 15
         exit(atoi(argv[1]));
     }
     else {
         do {
-            w = waitpid(cpid, &status, FLAGS);
-            fprintf(stderr,"parent= %d, child=%d, status=%d, w=%d\n", getpid(), cpid, status, w); //line A
+            w = waitpid(cpid, &status, WNOHANG);
+            printf("parent= %d, child=%d, status=%d, w=%d\n", getpid(), cpid, status, w); //line A
             if (w==-1) exit(0);
             if (w) {
-            if (WIFEXITED(status)) fprintf(stderr,"exited naturally, status = %d\n", WEXITSTATUS(status));
-            else if (WIFSIGNALED(status)) fprintf(stderr,"killed by signal: %d\n",WTERMSIG(status));
-            else if (WIFSTOPPED(status)) fprintf(stderr,"stopped by signal %d\n",WSTOPSIG(status));
-            else if (WIFCONTINUED(status))fprintf(stderr,"continued\n");
+            if (WIFEXITED(status)) printf("exited naturally, status = %d\n", WEXITSTATUS(status));
+            else if (WIFSIGNALED(status)) printf("killed by signal: %d\n",WTERMSIG(status));
+            else if (WIFSTOPPED(status)) printf("stopped by signal %d\n",WSTOPSIG(status));
+            else if (WIFCONTINUED(status))printf("continued\n");
             }
         }
-        while (CONDITIONALS);
+        while (CONDITIONALS1);
         exit(1);
     }
 }
