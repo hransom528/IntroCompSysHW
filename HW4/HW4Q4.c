f4103029dab3d02543f18231b18b0edacd9ccd70
Harris Ransom <hransom528@protonmail.com>
2024-04-15 23:13:44 -0400

    Finalized HW4 code

--- HW4/HW4Q4.c
+++ HW4/HW4Q4.c
@@ -7,27 +7,47 @@
 #define n 3 // Number of threads
 
 // Signal handler for SIGINT
-void sigHandler(int sig) {
-    // Print message
-    printf("Caught signal %d\n", sig);
+void sigHandler(int sig, siginfo_t *info, void *extra) {
+    printf("Caught signal %d in %ld\n", sig, pthread_self());
+}
 
-    // Exit
-    exit(0);
+// Sets up signal handler with sigaction
+void setSigHandler(void) {
+    struct sigaction action;
+    action.sa_flags = SA_SIGINFO; 
+    action.sa_sigaction = sigHandler;
+
+    if (sigaction(SIGINT, &action, NULL) == -1) { 
+        perror("sigusr: sigaction");
+        _exit(1);
+    }
+}
+
+// Signal masking
+void maskSig(void) {
+    sigset_t sigset; 
+    sigemptyset(&sigset);
+    sigaddset(&sigset, SIGINT);
+    pthread_sigmask(SIG_BLOCK, &sigset, NULL);
 }
 
 // Thread 1 function
+// Signal handling: default
 void *func1(void *arg) {
     signal(SIGINT, SIG_DFL);
-    for (int i=0; i < 10; i++) {
+
+    for (int i=0; i < 5; i++) {
         printf("Hello from func1\n");
-        sleep(1);
+        sleep(2);
     }
     return NULL;
 }
 
 // Thread 2 function
+// Signal handling: ignore (sigmask)
 void *func2(void *arg) {
-    signal(SIGINT, SIG_IGN);
+    maskSig();
+
     for (int i=0; i < 5; i++) {
         printf("Hello from func2\n");
         sleep(2);
@@ -36,37 +56,40 @@ void *func2(void *arg) {
 }
 
 // Thread 3 function
+// Signal handling: custom handler
 void *func3(void *arg) {
-    signal(SIGINT, sigHandler);
-    for (int i=0; i < 2; i++) {
+    setSigHandler();
+
+    for (int i=0; i < 5; i++) {
         printf("Hello from func3\n");
-        sleep(5);
+        sleep(2);
     }
     return NULL;
 }
 
 // MAIN
 int main() {
-    // Set up threads
     pthread_t thread1, thread2, thread3;
+
+    // Create threads
     pthread_create(&thread1, NULL, (void *) func1, NULL);
     pthread_create(&thread2, NULL, (void *) func2, NULL);
     pthread_create(&thread3, NULL, (void *) func3, NULL);
 
-    // TODO: Implement pthread_sigmask()
+    sleep(1);
 
     // Signal experiments
-    sleep(1);
     printf("Before SIGINT signals\n");
     //kill(getpid(), SIGINT);
     //pthread_kill(thread1, SIGINT);
     //pthread_kill(thread2, SIGINT);
-    //pthread_kill(thread3, SIGINT);
+    pthread_kill(thread3, SIGINT);
     printf("After SIGINT signals\n");
 
     // Wait for threads to finish
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
     pthread_join(thread3, NULL);
+    pthread_exit(NULL);
     return 0;
 }
